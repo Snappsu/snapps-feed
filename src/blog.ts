668d5a4d7d115ee799536a1269ff5a8386f7241a
snapps <lead.dumbass@snapps.dev>
2026-09-20 21:28:03 -0700

    remove author portion from embed

--- src/blog.ts
+++ src/blog.ts
@@ -314,25 +314,6 @@ export class Entry {
                     {
                     "type": 10,
                     "content": `-# Published: ${this.published}`
-                    },
-                    {
-                    "type": 14,
-                    "divider": true
-                    },
-                    {
-                    "type": 9,
-                    "components": [
-                        {
-                        "type": 10,
-                        "content": `## **About the Author**\n${env.BLOG_INFO.AUTHOR.DESC}`
-                        }
-                    ],
-                    "accessory": {
-                        "type": 11,
-                        "media": {
-                        "url": env.BLOG_INFO.AUTHOR.ICON
-                        }
-                    }
                     }
                 ]
             }
