bf794877c94e2b22b2e52668e894a68e994adac6
snapps <lead.dumbass@snapps.dev>
2026-09-20 21:41:35 -0700

    wrong format...

--- src/blog.ts
+++ src/blog.ts
@@ -313,7 +313,7 @@ export class Entry {
                     },
                     {
                     "type": 10,
-                    "content": `-# Published: <t:${this.published.valueOf().toString().slice(0, -3)}:R>`
+                    "content": `-# Published: <t:${this.published.valueOf().toString().slice(0, -3)}:f>`
                     }
                 ]
             }
